37706b02fa4f1a6ac74485bf4b96df6e755de36e
Yash Singhal <yashsinghal9886@gmail.com>
2024-03-16 12:00:13 +0530

    authentication using clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark,neobrutalism,shadesOfPurple } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +16,17 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
+    <ClerkProvider
+    appearance={{
+      baseTheme: dark
+    }}
+    >
+      
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
